a58afd7ff45355bed53a88635ef12296cbe2e956
harish <harish@osmosys.co>
2024-02-12 18:49:24 +0530

    add graphql implementation

---
+++ Pinestem/API/Types/InputType/TaskInputType.cs
@@ -0,0 +1,22 @@
+﻿using DAL.Models;
+using GraphQL.Types;
+
+namespace API.Types.InputType
+{
+    public class TaskInputType : InputObjectGraphType<TaskDetails>
+    {
+        public TaskInputType()
+        {
+            Name = "Input";
+            Field(x => x.TaskID);
+            Field(x => x.TaskName);
+            Field(x => x.ProjectCode);
+            Field(x => x.ProjectName);
+            Field(x => x.AssignedTo);
+            Field(x => x.AssignedBy);
+            Field(x => x.StatusType);
+            Field(x => x.ExpectedHours);
+            Field(x => x.BillableHours);
+        }
+    }
+}
