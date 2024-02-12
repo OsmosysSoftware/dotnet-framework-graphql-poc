38fe78f9c0d78540e4e7741d107c586ac80eee0b
harish <harish@osmosys.co>
2024-02-12 18:45:31 +0530

    add graphql implementation

---
+++ Pinestem/API/Controllers/GraphQLController.cs
@@ -0,0 +1,48 @@
+﻿using GraphQL.Types;
+using GraphQL;
+using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using API.Models;
+
+namespace API.Controllers
+{
+    public class GraphQLController : ApiController
+    {
+        private readonly IDocumentExecuter _documentExecuter;
+        private readonly ISchema _schema;
+
+        public GraphQLController(ISchema schema, IDocumentExecuter documentExecuter)
+        {
+            _schema = schema;
+            _documentExecuter = documentExecuter;
+        }
+
+        [HttpPost]
+        public async Task<IHttpActionResult> Post([FromBody] GraphQLQuery query)
+        {
+            if (query == null)
+            {
+                throw new ArgumentNullException(nameof(query));
+            }
+
+            var inputs = query.Variables.ToInputs();
+            var executionOptions = new ExecutionOptions
+            {
+                Schema = _schema,
+                Query = query.Query,
+                Inputs = inputs
+            };
+
+            var result = await _documentExecuter
+                .ExecuteAsync(executionOptions);
+
+            if (result.Errors?.Count > 0)
+            {
+                return BadRequest();
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ Pinestem/DAL/GraphQL/Interface/ITaskRepository.cs
@@ -0,0 +1,15 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.GraphQL.Interface
+{
+    public interface ITaskRepository
+    {
+        IEnumerable<TaskDetails> GetAll();
+        TaskDetails GetById(int id);
+    }
+}
